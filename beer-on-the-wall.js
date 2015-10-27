1ae4ac1a13f4bbf74e75129b036b3b8031b43c6e
alexnishioki <alexjnishioki@gmail.com>
2015-10-26 22:05:59 -0700

    Create beer-on-the-wall.js

---
+++ beer-on-the-wall.js
@@ -0,0 +1,13 @@
+var x = 1
+var y = 0
+for(var i = 99; i > 0; i--) {
+if(i !== 1){
+   console.log(i + " " + "bottles of beer on the wall " + i + " bottles of beer, " + " take one down and pass it around ", + i-1
+   + " bottles of beer on the wall. ") 
+       }if (i===1) {
+           console.log(x + " bottle of beer on the wall " + x + " bottle of beer, take one down pass it around " + y + " bottles of beer on the wall")
+   
+       } if(i === 2) {
+           console.log(i + " " + "bottles of beer on the wall " + i + " bottles of beer, " + " take one down and pass it around ", + i-1 + " bottle of beer on the wall. ") 
+       }
+}
